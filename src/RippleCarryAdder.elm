f19b6f7995cdefa5211c3b45e58421387040d5a2
Alex Warrington <awarrington0895@gmail.com>
2020-10-28 19:06:03 -0500

    Add first module and testing

---
+++ src/RippleCarryAdder.elm
@@ -0,0 +1,116 @@
+module RippleCarryAdder exposing (andGate, orGate, inverter, halfAdder, fullAdder, rippleCarryAdder, Binary, digits)
+
+import Bitwise
+import Array
+
+type alias Binary = 
+  { d0 : Int
+  , d1 : Int
+  , d2 : Int
+  , d3 : Int
+  }
+
+andGate a b = Bitwise.and a b
+
+orGate a b =
+  Bitwise.or a b
+
+inverter a =
+  case a of
+     0 -> 1
+     1 -> 0
+     _ -> -1
+
+halfAdder a b =
+  let
+      d = orGate a b
+      e = andGate a b |>inverter
+      sumDigit = andGate d e
+      carryOut = andGate a b
+  in
+  { carry = carryOut
+  , sum = sumDigit
+  }
+  
+fullAdder a b carryIn =
+  let
+    firstResult = halfAdder b carryIn
+    secondResult = halfAdder a firstResult.sum
+    finalCarry = orGate firstResult.carry secondResult.carry
+  in
+  { carry = finalCarry
+  , sum = secondResult.sum
+  }
+
+rippleCarryAdder a b carryIn =
+  let
+    firstSignal = extractDigits a
+    
+    secondSignal = extractDigits b
+
+    firstResult = fullAdder firstSignal.d3 secondSignal.d3 carryIn
+
+    secondResult = fullAdder firstSignal.d2 secondSignal.d2 firstResult.carry
+
+    thirdResult = fullAdder firstSignal.d1 secondSignal.d1 secondResult.carry
+    
+    finalResult = fullAdder firstSignal.d0 secondSignal.d0 thirdResult.carry
+
+  in
+  [ finalResult, thirdResult, secondResult, firstResult ]
+    |> List.map .sum
+    |> (::) finalResult.carry
+    |> numberFromDigits
+
+stringToInt string =
+  String.toInt string
+    |> Maybe.withDefault -1
+
+arrayToRecord array =
+  let
+    firstElement = 
+      Array.get 0 array |> Maybe.withDefault -1
+
+    secondElement =
+      Array.get 1 array |> Maybe.withDefault -1
+
+    thirdElement =
+      Array.get 2 array |> Maybe.withDefault -1
+
+    fourthElement = 
+      Array.get 3 array |> Maybe.withDefault -1
+
+  in
+  { d0 = firstElement
+  , d1 = secondElement
+  , d2 = thirdElement
+  , d3 = fourthElement
+  }
+
+extractDigits number =
+  digits number
+    |> padZeroes 4
+    |> Array.fromList
+    |> arrayToRecord
+
+numberFromDigits digitsList =
+  List.foldl (\digit number -> digit + 10 * number) 0 digitsList
+
+digits number =
+  let
+    getDigits n =
+      if n == 0 then
+        []
+
+      else 
+        remainderBy 10 n :: getDigits (n // 10)
+  in
+  getDigits number
+    |> List.reverse
+
+padZeroes total list =
+  let
+    numberOfZeroes =
+      total - List.length list
+  in
+  List.repeat numberOfZeroes 0 ++ list
